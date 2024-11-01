09a4798daacc687e132dddf5a7f1278dba411fc6
Dinuwamin <pakayasudda11@gmail.com>
2024-11-01 14:49:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "ZDc2VZqA#47tgLjAewJLgEn_GTdnhgIF7_mAnSUPQ4rIrMfCoV5c",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/106251140?v=4",
-ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am YourName i am alive now!",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am KING ASH MD i am alive now!",
 };

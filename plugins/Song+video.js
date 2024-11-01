20c327f6bde98450668d1f38e88838912cbc649d
Dinuwamin <pakayasudda11@gmail.com>
2024-11-01 21:12:50 +0530

    Update Song+video.js

--- plugins/Song+video.js
+++ plugins/Song+video.js
@@ -1,6 +1,6 @@
-const {cmd , commands} = require("../command")
-const fg = require("api-dylux")
-const yts = require("yt-search")
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
 
 
 cmd({
@@ -19,7 +19,7 @@ const url = data.url
 let desc = `
 🌸 *KING ASH MD SONG DOWNLOADER* 🌸
 
-tittle: ${data.tittle}
+tittle: ${data.title}
 discription: ${data.discription}
 time: ${data.timestamp}
 ago: ${data.ago}
@@ -32,12 +32,59 @@ await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:m
 //download audio 
 
 let down = await fg.yta(url)
-let downloadUrl = dowm.dl_url
+let downloadUrl = down.dl_url
 
 //send audio message
 await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
 
 
+
+
+  
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
+//==========video-dl==========
+
+cmd({
+    pattern: "video",
+    desc: "download videos",
+    category: "download ",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me a song Url Or Tittle 🚫")
+const search = await yts(q) 
+const data = search.videos[0];
+const url = data.url 
+
+let desc = `
+🌸 *KING ASH MD VIDEO DOWNLOADER* 🌸
+
+tittle: ${data.title}
+discription: ${data.discription}
+time: ${data.timestamp}
+ago: ${data.ago}
+views: ${data.views}
+
+CREATE BY OLD KING LOKU ASH 👨‍💻✅
+`
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+    
+//download video 
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video message
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+
+await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"video/mp4",fileName:deta.title + ".mp4"},{quoted:mek})
 
 
   

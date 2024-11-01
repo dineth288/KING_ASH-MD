96ef4f0e65a73c970d4266870e7dc45479596fb3
Dinuwamin <pakayasudda11@gmail.com>
2024-11-01 18:29:21 +0530

    Update Song+video.js

--- plugins/Song+video.js
+++ plugins/Song+video.js
@@ -1,6 +1,8 @@
 const {cmd , commands} = require('../command')
 const fg = require(`api-dylux`)
 const yts = require(`yt-search`)
+
+
 cmd({
     pattern: "song",
     desc: "download songs",
@@ -9,8 +11,14 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+if(!q) return reply("Please give me a Url Or Titel")
+const search = await yts(q) 
+const data = search.videos[0];
+const url = data.url 
 
-}catch(e){
+}
+
+catch(e){
 console.log(e)
  reply(`${e}`)
 }

9a3f137622841987aa79276dd6e2af66ba048c73
Dinuwamin <pakayasudda11@gmail.com>
2024-11-02 10:32:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,4 @@ SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/nfX5yZ6/IMG-20241101-WA0114.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello,🖐️ I' am Ash Md I Am Alive Now ⚜🌸👀",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-
-
-
 };
